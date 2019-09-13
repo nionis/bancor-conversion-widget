d54c0d3419b56a6f9ad144b8975aba4ebaee75d3
Ashley Oytac <ashlinkaoyt@gmail.com>
2019-09-14 02:30:21 +0300

    implement popup

---
+++ src/components/PopUp.svelte
@@ -0,0 +1,48 @@
+<script>
+  import PopUpStep from "./PopUpStep.svelte";
+  import useCssVars from "svelte-css-vars";
+
+  export let colors;
+
+  export let bgColor = "white";
+  export let fontColor = "black";
+  export let borderColor = "grey";
+  $: cssVars = {
+    borderColor: colors.containerBorder,
+    bgColor: colors.containerBg
+  };
+</script>
+
+<style>
+  /* .container {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    background-color: rgba(0, 0, 0, 0.48);
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    z-index: 5;
+  } */
+  .popup {
+    border-radius: 10px;
+    background-color: var(--bgColor);
+    border: var(--borderColor) solid 3px;
+    flex-direction: column;
+    width: 375px;
+    height: 325px;
+    display: flex;
+    position: absolute;
+  }
+</style>
+
+<!-- <div class="container"> -->
+<div class="popup" use:useCssVars={cssVars}>
+  <PopUpStep {colors} done={true} />
+  <PopUpStep {colors} />
+  <PopUpStep {colors} active={false} />
+  <PopUpStep {colors} active={false} border={false} />
+</div>
+<!-- </div> -->
---
+++ src/components/PopUpStep.svelte
@@ -0,0 +1,61 @@
+<script>
+  import Button from "./Button.svelte";
+  import useCssVars from "svelte-css-vars";
+  import MdCheck from "svelte-icons/md/MdCheck.svelte";
+
+  export let colors;
+  export let border = true;
+  export let active = true;
+  export let done = false;
+
+  let buttonBg = colors.buttonBg;
+  let buttonFont = colors.buttonFont;
+  let buttonBorder = colors.buttonBorder;
+
+  $: cssVars = {
+    borderColor: colors.containerBorder,
+    fontColor: colors.containerFont,
+    border: border ? "3px" : "0px",
+    opacity: active ? 1 : 0.5,
+    iconColor: colors.buttonBg
+  };
+</script>
+
+<style>
+  .container {
+    border-bottom: var(--borderColor) solid var(--border);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-grow: 1;
+    padding-right: 20px;
+    color: var(--fontColor);
+    opacity: var(--opacity);
+  }
+  .step {
+    padding: 20px;
+    padding-right: 0;
+    color: var(--fontColor);
+    text-align: center;
+    width: 30px;
+    max-height: 30px;
+  }
+  .icon {
+    color: var(--iconColor);
+  }
+</style>
+
+<div class="container" use:useCssVars={cssVars}>
+  <div class="step">
+    {#if done}
+      <div class="icon">
+        <MdCheck />
+      </div>
+    {:else}1{/if}
+
+  </div>
+  <div class="text">This is the question?</div>
+  <Button bgColor={buttonBg} fontColor={buttonFont} borderColor={buttonBorder}>
+    Accept
+  </Button>
+</div>
