e070ee395ae3bc6cb2ff7fe797c2f006b165507e
Ashley Oytac <ashlinkaoyt@gmail.com>
2019-11-28 21:31:04 +0200

    (fix) cleanup code

--- src/components/ConvertSteps/Success.svelte
+++ src/components/ConvertSteps/Success.svelte
@@ -1,19 +1,17 @@
 <script>
   /*
-    SelectTokens
-    A list of tokens which can be used to select a send token / receive token
+    Success
+    A success view once the conversion is complete.
   */
 
   import { createEventDispatcher } from "svelte";
   import useCssVars from "svelte-css-vars";
-  import Select from "svelte-select";
   import MdClose from "svelte-icons/md/MdClose.svelte";
   import MdCheck from "svelte-icons/md/MdCheck.svelte";
-  import Item from "../SelectTokens/Item.svelte";
   import Icon from "../Icon.svelte";
-  import Required from "../../utils/Required";
   import Button from "../Button.svelte";
   import Link from "../Link.svelte";
+  import Required from "../../utils/Required";
 
   export let bgColor = Required("bgColor");
   export let fontColor = Required("fontColor");
@@ -21,22 +19,13 @@
   export let disabledFont = Required("disabledFont");
   export let buttonBgColor = Required("buttonBgColor");
   export let buttonFontColor = Required("buttonFontColor");
-  //   export let open = true;
-  //   export let loading = false;
 
   const dispatch = createEventDispatcher();
 
-  // this is a pseudo item to show "loading" withing Select
-  //   const loadingItem = {
-  //     value: "__LOADING",
-  //     label: "loading"
-  //   };
+  const onClose = () => {
+    dispatch("close");
+  };
 
-  //   const onClose = () => {
-  //     open = false;
-
-  //     dispatch("close");
-  //   };
   $: cssVars = {
     bgColor,
     disabledFont,
@@ -57,11 +46,13 @@
     width: 100%;
     background: var(--bgColor);
   }
+
   .closeContainer {
     display: flex;
     width: 100%;
     justify-content: flex-end;
   }
+
   .iconContainer {
     width: 100px;
     border: 7px solid var(--successColor);
@@ -73,13 +64,16 @@
     transform: rotate(270deg);
     margin-bottom: 40px;
   }
+
   h1 {
     font-weight: normal;
     color: var(--fontColor);
   }
+
   .btnContainer {
     margin-bottom: 24px;
   }
+
   .textAndIcon {
     display: flex;
     justify-content: center;
@@ -100,17 +94,13 @@
         <MdCheck />
       </Icon>
     </div>
-
     <h1>Conversion</h1>
     <h1>Successful!</h1>
     <Link url="https://etherscan.io" fontColor={disabledFont}>etherscan</Link>
   </div>
-
   <div class="btnContainer">
     <Button bgColor={buttonBgColor} fontColor={buttonFontColor}>Done</Button>
-
   </div>
-
 </div>
 
 <!-- example -->
